c39214dcd33425c0a10bc8c5c13341d4d8d24d82
batuhanbulut0614 <113903075+batuhanbulut0614@users.noreply.github.com>
2022-09-28 12:01:21 +0300

    Initial commit

---
+++ src/daireHesap/Main.java
@@ -0,0 +1,18 @@
+package daireHesap;
+
+public class Main {
+
+	public static void main(String[] args) {
+        double yariCap=4;
+        double pi=3.1416;
+        double daireAlan=0;
+        double daireCevre=0;
+        daireAlan=pi*yariCap*yariCap;
+        System.out.println("Daire'nin Alanı ="+daireAlan);
+        daireCevre=2*pi*yariCap;
+        System.out.println("Daire'nin Çevresi ="+daireCevre);
+       
+
+	}
+
+}
